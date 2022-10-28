588391f051166b8b9927850446d445f92331aba4
Janek <eunho.moon@gmail.com>
2022-10-29 00:12:03 +0900

    JDBC-2

--- src/main/java/hello/jdbc/repository/MemberRepositoryVO.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryVO.java
@@ -4,15 +4,13 @@ import hello.jdbc.connection.DBConnectionUtil;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 @Slf4j
 public class MemberRepositoryVO {
 
     public Member save(Member member) throws SQLException {
-        String sql = "INSERT INTO member(member_id, money VALUES (?, ?)";
+        String sql = "INSERT INTO member(member_id, money) VALUES (?, ?)";
 
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -22,13 +20,40 @@ public class MemberRepositoryVO {
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, member.getMemberId());
             pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
             log.error("db error", e);
             throw e;
         } finally {
-            pstmt.close();
-            con.close();
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
         }
     }
 
